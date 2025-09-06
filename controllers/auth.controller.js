8419aac428b8e238fb8ca24b47fce31427c46cb2
Seyi Adeniyi <adeniyiemmanuel042@gmail.com>
2025-09-07 00:05:31 +0100

    loggin issue

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -76,11 +76,12 @@ export default async function companyAndUserAuth(req,res,next) {
 
     // ✅ Set cookie
     res.cookie('token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'Lax',
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    });
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+});
+
 
     // Exclude password before sending
 const { password: _, ...userWithoutPassword } = user;
